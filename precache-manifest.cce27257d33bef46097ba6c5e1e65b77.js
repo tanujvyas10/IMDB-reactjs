79a55f2ef0bb238a33abd8fe0667ee8f1552b71b
tanuj <tanujvyas10@gmail.com>
2020-06-26 17:55:57 +0530

    Updates

---
+++ precache-manifest.cce27257d33bef46097ba6c5e1e65b77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "152d177b215b6cce4f08100cb424ba75",
+    "url": "/tanujvyas10.github.io/React-movie-app/index.html"
+  },
+  {
+    "revision": "e2614e6ddffa5e7957e6",
+    "url": "/tanujvyas10.github.io/React-movie-app/static/css/main.5b53e8c5.chunk.css"
+  },
+  {
+    "revision": "39fc647e1deb61926b2c",
+    "url": "/tanujvyas10.github.io/React-movie-app/static/js/2.d58b4a76.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tanujvyas10.github.io/React-movie-app/static/js/2.d58b4a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2614e6ddffa5e7957e6",
+    "url": "/tanujvyas10.github.io/React-movie-app/static/js/main.10d7f836.chunk.js"
+  },
+  {
+    "revision": "b7c7934ee42e897c2da4",
+    "url": "/tanujvyas10.github.io/React-movie-app/static/js/runtime-main.716f4ccc.js"
+  }
+]);
